036aa7cf3ac877ca6621e87eb15256ca8ee79193
Lucas <lucasoliveirars22@gmail.com>
2023-02-25 11:31:49 -0300

    add swagger documentation

--- setup/urls.py
+++ setup/urls.py
@@ -7,6 +7,24 @@ from django.conf.urls.static import static
 from school.views import StudentsViewSet, TeacherViewSet, CoursesViewSet, RegistrationViewSet, ListRegistrationsStudentViewSet, ListEnrolledStudentsViewSet
 
 from rest_framework import routers
+from rest_framework import permissions
+
+#swagger
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API-school",
+      default_version='v1',
+      description="A course platform management API",
+      terms_of_service="#",
+      contact=openapi.Contact(email="owner@apischool.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 router = routers.DefaultRouter()
 router.register('students', StudentsViewSet, basename='Students')
@@ -18,5 +36,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls) ),
     path('students/<int:pk>/registrations/', ListRegistrationsStudentViewSet.as_view()),
-    path('courses/<int:pk>/registrations/', ListEnrolledStudentsViewSet.as_view())
+    path('courses/<int:pk>/registrations/', ListEnrolledStudentsViewSet.as_view()),
+    path('swagger/documentation/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
